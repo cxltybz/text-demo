5be04aead6af3449e20d21fbc3e1e6c262cba3ed
cxlt <1324071204@qq.com>
2023-09-17 22:03:29 +0800

    保存生命周期

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,19 +1,36 @@
 import { Component } from "react";
+import PropTypes from "prop-types";
 
-// export default class TodoItems extends Component {
-//     constructor(props) {
-//         super(props);
-//         this.handleClick = this.handleClick.bind(this);
-//     }
-//     handleClick() {
-//         this.props.deleteItem(this.props.index);
-//     }
-//     render() {
-//         return <div onClick={this.handleClick}>{this.props.cotent}</div>;
-//     }
-// }
-const TodoItems = props => {
-    const { content, onDelete } = props;
-    return <div onClick={onDelete}>{content}</div>;
+class TodoItems extends Component {
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick() {
+        const { deleteItem, index } = this.props;
+        deleteItem(index);
+    }
+    render() {
+        const { cotent, test } = this.props;
+        return (
+            <div onClick={this.handleClick}>
+                {test}-{cotent}
+            </div>
+        );
+    }
+}
+// const TodoItems = props => {
+//     const { content, onDelete } = props;
+//     return <div onClick={onDelete}>{content}</div>;
+// };
+TodoItems.propTypes = {
+    test: PropTypes.string.isRequired,
+    cotent: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    deleteItem: PropTypes.func,
+    index: PropTypes.number,
 };
+TodoItems.defaultProps = {
+    test: "hello word",
+};
+
 export default TodoItems;
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,99 +1,118 @@
 import React, { useState } from "react";
 import TodoItems from "./TodoItem";
+import Test from "./Test";
 import "./style.css";
-// class TodoList extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             inputValue: "", // 输入框数据
-//             list: [], // 列表数据
-//         };
-//     }
-//     // 输入框事件
-//     handleInputChange(e) {
-//         this.setState({
-//             inputValue: e.target.value,
-//         });
-//     }
-//     // 提交事件
-//     onOk() {
-//         this.setState({
-//             list: [...this.state.list, this.state.inputValue],
-//             inputValue: "",
-//         });
-//     }
-//     // 删除事件
-//     handleItemDelete(index) {
-//         const list = [...this.state.list];
-//         list.splice(index, 1);
-//         this.setState({
-//             list: list,
-//         });
-//     }
-//     render() {
-//         return (
-//             <>
-//                 <div>
-//                     <input
-//                         value={this.state.inputValue}
-//                         onChange={e => this.handleInputChange(e)}
-//                     />
-//                     <button onClick={() => this.onOk()}>提交</button>
-//                 </div>
-//                 <ul>
-//                     {this.state.list.map((item, index) => (
-//                         <TodoItems
-//                             cotent={item}
-//                             index={index}
-//                             key={index}
-//                             deleteItem={this.handleItemDelete.bind(this)}
-//                         />
-//                     ))}
-//                 </ul>
-//             </>
-//         );
-//     }
-// }
 
-const TodoList = () => {
-    // 列表
-    const [list, setList] = useState([]);
-    const [inputValue, setInputValue] = useState("");
-    return (
-        <>
-            {/* 注释 */}
-            {
-                // 单行注释
-            }
-            <div>
-                <label htmlFor="name">内容</label>
-                <input
-                    className="input"
-                    id="name"
-                    value={inputValue}
-                    onChange={e => setInputValue(e.target.value)}
-                />
-                <button
-                    onClick={() => {
-                        setList([...list, inputValue]);
-                        setInputValue("");
-                    }}
-                >
-                    提交
-                </button>
-            </div>
-            <ul>
-                {list.map((item, index) => (
-                    <TodoItems
-                        key={index}
-                        content={item}
-                        index={index}
-                        onDelete={() => setList(pre => pre.splice(index, 1))}
+class TodoList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            inputValue: "", // 输入框数据
+            list: [], // 列表数据
+        };
+    }
+    // 输入框事件
+    handleInputChange(e) {
+        this.setState(() => ({
+            inputValue: e.target.value,
+        }));
+    }
+    // 提交事件
+    onOk() {
+        this.setState(prevState => ({
+            list: [...prevState.list, prevState.inputValue],
+            inputValue: "",
+        }));
+    }
+    // 删除事件
+    handleItemDelete(index) {
+        this.setState(prevState => {
+            const list = [...prevState.list];
+            list.splice(index, 1);
+            return { list };
+        });
+    }
+    // 获取列表子项
+    getTodoItem() {
+        return this.state.list.map((item, index) => (
+            <TodoItems
+                cotent={item}
+                index={index}
+                key={index}
+                deleteItem={this.handleItemDelete.bind(this)}
+            />
+        ));
+    }
+    // 在组件即将被挂载到页面的时刻自动执行
+    componentWillMount() {
+        console.log("componentWillMount");
+    }
+    // 组件在被挂载到页面之后，自动被执行
+    componentDidMount() {
+        console.log("componentDidMount");
+    }
+    // 组件更新前执行，需要返回boolean值
+    shouldComponentUpdate() {
+        console.log("shouldComponentUpdate");
+        return true;
+    }
+    render() {
+        console.log("render");
+        return (
+            <>
+                <div>
+                    <input
+                        value={this.state.inputValue}
+                        onChange={e => this.handleInputChange(e)}
                     />
-                ))}
-            </ul>
-        </>
-    );
-};
+                    <button onClick={() => this.onOk()}>提交</button>
+                </div>
+                <ul>{this.getTodoItem()}</ul>
+                <Test content={this.state.inputValue} />
+            </>
+        );
+    }
+}
+
+// const TodoList = () => {
+//     // 列表
+//     const [list, setList] = useState([]);
+//     const [inputValue, setInputValue] = useState("");
+//     return (
+//         <>
+//             {/* 注释 */}
+//             {
+//                 // 单行注释
+//             }
+//             <div>
+//                 <label htmlFor="name">内容</label>
+//                 <input
+//                     className="input"
+//                     id="name"
+//                     value={inputValue}
+//                     onChange={e => setInputValue(e.target.value)}
+//                 />
+//                 <button
+//                     onClick={() => {
+//                         setList([...list, inputValue]);
+//                         setInputValue("");
+//                     }}
+//                 >
+//                     提交
+//                 </button>
+//             </div>
+//             <ul>
+//                 {list.map((item, index) => (
+//                     <TodoItems
+//                         key={index}
+//                         content={item}
+//                         index={index}
+//                         onDelete={() => setList(pre => pre.splice(index, 1))}
+//                     />
+//                 ))}
+//             </ul>
+//         </>
+//     );
+// };
 
 export default TodoList;
