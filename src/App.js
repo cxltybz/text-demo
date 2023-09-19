6cd8d851fc605a0b6a7b4f4824166c0ebbc3e3eb
LAPTOP-N75N2CF0\86136 <1324071204@qq.com>
2023-09-19 22:35:03 +0800

    css过度动画

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
-import TodoList from "./TodoList";
-const App = () => {
-    return <TodoList />;
-};
+import React, { Component, Fragment } from 'react';
+import './style.css';
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: true,
+    };
+  }
+
+  onShow() {
+    this.setState(() => ({
+      show: !this.state.show,
+    }));
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <div className={this.state.show ? 'show' : 'hide'}>hello</div>
+        <button onClick={this.onShow.bind(this)}>toggle</button>
+      </Fragment>
+    );
+  }
+}
 
 export default App;
