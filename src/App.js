7ca84664b7cc50d087867760cdbada99f00798c7
LAPTOP-N75N2CF0\86136 <1324071204@qq.com>
2023-09-20 18:38:10 +0800

    react过渡动画场景transition

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
 import React, { Component, Fragment } from 'react';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import './style.css';
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      show: true,
+      list: [],
     };
+    this.nodeRef = React.createRef();
+  }
+  // 添加子元素过渡的事件
+  handleAddItem() {
+    this.setState((preState) => ({
+      list: [...preState.list, 'item'],
+    }));
   }
 
-  onShow() {
-    this.setState(() => ({
-      show: !this.state.show,
-    }));
+  // 显示后的颜色事件
+  enterdColor() {
+    const emlemnt = this.nodeRef.current;
+    emlemnt.style.color = 'blue';
   }
 
   render() {
     return (
       <Fragment>
-        <div className={this.state.show ? 'show' : 'hide'}>hello</div>
-        <button onClick={this.onShow.bind(this)}>toggle</button>
+        <TransitionGroup>
+          {this.state.list.map((item, index) => (
+            <CSSTransition
+              in={this.state.show}
+              timeout={1000}
+              classNames="fade"
+              appear
+              nodeRef={this.nodeRef}
+              unmountOnExit
+              onEntered={this.enterdColor.bind(this)}
+              key={index}
+            >
+              <div ref={this.nodeRef}>{item}</div>
+            </CSSTransition>
+          ))}
+        </TransitionGroup>
+
+        <button onClick={this.handleAddItem.bind(this)}>toggle</button>
       </Fragment>
     );
   }
